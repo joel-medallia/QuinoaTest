eab5c850abc5d03aeb294506aa09a6470d26574f
Joe Leung <joel@medallia.com>
2022-07-24 08:50:08 -0700

    First Release

---
+++ QuinoaTest.xcframework/ios-arm64_x86_64-simulator/QuinoaTest.framework/Headers/QuinoaTest.h
@@ -0,0 +1,18 @@
+//
+//  QuinoaTest.h
+//  QuinoaTest
+//
+//  Created by Joe Leung on 7/22/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuinoaTest.
+FOUNDATION_EXPORT double QuinoaTestVersionNumber;
+
+//! Project version string for QuinoaTest.
+FOUNDATION_EXPORT const unsigned char QuinoaTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuinoaTest/PublicHeader.h>
+
+
